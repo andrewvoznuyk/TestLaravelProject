b433aec389c9ad5e133262a836c81f4b209d6f2a
Nika <kn201_mvv@student.ztu.edu.ua>
2024-08-14 18:11:28 +0300

    [FEATURE]
    Create api routing file and other configs such a proxies for doctrine.
     - Composer.json and lock files are provided.

---
+++ routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\BookController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::prefix('authors')->group(function () {
+    Route::post('/', [AuthorController::class, 'store']);
+    Route::delete('/{author}', [AuthorController::class, 'destroy']);
+    Route::get('/', [AuthorController::class, 'index']);
+    Route::get('/{author}', [AuthorController::class, 'show']);
+});
+
+Route::prefix('books')->group(function () {
+    Route::post('/', [BookController::class, 'store']);
+    Route::delete('/{book}', [BookController::class, 'destroy']);
+    Route::get('/', [BookController::class, 'index']);
+    Route::get('/{book}', [BookController::class, 'show']);
+});
